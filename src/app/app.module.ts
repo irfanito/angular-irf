8305992ce2cd8be7935bbd7b32bea2f573f0cc5d
irfanito <irfane.salim@gmail.com>
2019-11-15 11:48:41 +0100

    internationalization

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,6 @@
-import { NgModule } from "@angular/core";
+import { NgModule, LOCALE_ID } from "@angular/core";
+import { registerLocaleData } from '@angular/common';
+import localeFr from '@angular/common/locales/fr';
 import { BrowserModule } from "@angular/platform-browser";
 import { ReactiveFormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
@@ -12,12 +14,17 @@ import { MessageComponent } from "./message/message.component";
 import { TextComponent } from "./text/text.component";
 import { DefaultInvalidDirective } from "./default-invalid.directive";
 
+registerLocaleData(localeFr, 'fr');
+
 @NgModule({
   imports: [
     BrowserModule,
     ReactiveFormsModule,
     HttpClientModule,
     RouterModule.forRoot([{ path: "", component: EvolutionComponent }])
+  ],
+  providers: [
+    { provide: LOCALE_ID, useValue: "fr" }
   ],
   declarations: [
     AppComponent,
